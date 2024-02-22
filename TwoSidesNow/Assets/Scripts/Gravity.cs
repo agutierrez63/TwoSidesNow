e7bf70c5c64e495776129c9a224c68a4f1f8fa67
Adrian Gutierrez <46102354+agutierrez63@users.noreply.github.com>
2024-02-22 13:40:53 -0800

    Physics working
    Fixed the gravity switching physics when entering a trigger.
    
    TODO: Fix jumping with negative gravity

--- TwoSidesNow/Assets/Scripts/Gravity.cs
+++ TwoSidesNow/Assets/Scripts/Gravity.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-
-    [Header("Gravity Settings")]
-    // [SerializeField] private Transform _playerTransform;
-    // [SerializeField] private Rigidbody2D _rb;
-    // [SerializeField] private float _influence;
-    // [SerializeField] private float _intensity;
-    // [SerializeField] private float _playerDistance;
-
-    // private Vector2 _pullForce;
     private PlayerController _player;
     private Rigidbody2D _rb;
 
@@ -25,33 +17,12 @@ public class Gravity : MonoBehaviour
         _player = GetComponent<PlayerController>();
     }
 
-    private void Update()
+    private void OnTriggerExit2D(Collider2D collider)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (collider.CompareTag("Particles"))
         {
             _rb.gravityScale *= -1;
-            Rotation();
-        }
-
-        // OnTriggerEnter2D(_player._collider);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        /*if(other.CompareTag("Player"))
-        {
-            _playerDistance = Vector2.Distance(_playerRB.position, transform.position);
-            if (_playerDistance <= _influence)
-            {
-                _pullForce = (transform.position - _playerTransform.position).normalized / _playerDistance * _intensity;
-                _playerRB.AddForce(_pullForce, ForceMode2D.Force);
-                _playerRB.gravityScale *= -10;
-            }
-        }*/
-
-        if (other.CompareTag("Player"))
-        {
-            _rb.gravityScale *= -1;
+            Debug.Log("Gravity Scale: " + _rb.gravityScale);
             Rotation();
         }
     }
@@ -59,9 +30,9 @@ public class Gravity : MonoBehaviour
     private void Rotation()
     {
         if (_top == false)
-            transform.eulerAngles = new Vector2(180f, 0);
+            transform.eulerAngles = new Vector3(0, 0, 180f);
         else
-            transform.eulerAngles = Vector2.zero;
+            transform.eulerAngles = Vector3.zero;
 
         _player.facingRight = !_player.facingRight;
         _top = !_top;
--- TwoSidesNow/Assets/Scripts/PlayerController.cs
+++ TwoSidesNow/Assets/Scripts/PlayerController.cs
@@ -3,120 +3,121 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D), typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class PlayerController : MonoBehaviour
 {
-    // PARAMETERS: Player
+    [Header("Player Paramters")]
+    [SerializeField]
     private float _maxSpeed = 8.4f;
-    private float _jumpHeight = 12.5f;
+    [SerializeField]
+    private float _jumpHeight = 8.2f;
+    [SerializeField]
     private float _gravityScale = 1.0f;
+    [SerializeField]
+    private float _moveDirection = 0;
 
-    private Camera _mainCamera;
-    private Vector3 _cameraPos;
+    private bool _grounded = false;
+    private Rigidbody2D _rb;
+    private BoxCollider2D _mainCollider;
 
     public bool facingRight = true;
-    public float moveDirection = 0;
-    public bool grounded = false;
-
-    public BoxCollider2D _collider;
-    public Transform _t;
-
-    private Rigidbody2D _rb;
-    private CapsuleCollider2D _capsuleCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        _t = transform;
         _rb = GetComponent<Rigidbody2D>();
-        _capsuleCollider = GetComponent<CapsuleCollider2D>();
+        _mainCollider = GetComponent<BoxCollider2D>();
 
         _rb.freezeRotation = true;
         _rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
         _rb.gravityScale = _gravityScale;
 
-        facingRight = _t.localScale.x > 0;
-
-        if (_mainCamera)
-        {
-            _cameraPos = _mainCamera.transform.position;
-        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Movement controls
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (grounded || Mathf.Abs(_rb.velocity.x) > 0.01f))
-        {
-            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
-        }
-        else
-        {
-            if (grounded || _rb.velocity.magnitude < 0.01f)
-            {
-                moveDirection = 0;
-            }
-        }
-
-        // Change facing direction
-        if (moveDirection != 0)
-        {
-            if (moveDirection > 0 && !facingRight)
-            {
-                facingRight = true;
-                _t.localScale = new Vector3(Mathf.Abs(_t.localScale.x), _t.localScale.y, transform.localScale.z);
-            }
-            if (moveDirection < 0 && facingRight)
-            {
-                facingRight = false;
-                _t.localScale = new Vector3(-Mathf.Abs(_t.localScale.x), _t.localScale.y, _t.localScale.z);
-            }
-        }
-
-        // Jumping
-        if ((Input.GetKeyDown(KeyCode.W) || (Input.GetKeyDown(KeyCode.Space))) && grounded)
-        {
-            _rb.velocity = new Vector2(_rb.velocity.x, _jumpHeight);
-        }
-
-        // Camera follow
-        if (_mainCamera)
-        {
-            _mainCamera.transform.position = new Vector3(_t.position.x, _cameraPos.y, _cameraPos.z);
-        }
+        Jump();
     }
-
 
     void FixedUpdate()
     {
-        Bounds colliderBounds = _capsuleCollider.bounds;
-        float colliderRadius = _capsuleCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
+        Movement();
+    }
+
+    #region CheckGrounded
+    private void CheckGrounded()
+    {
+        Bounds colliderBounds = _mainCollider.bounds;
+        float colliderRadius = _mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
         Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
 
-        // Check if player is grounded
+        // Check if player is _grounded
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderRadius);
-        
-        //Check if any of the overlapping colliders are not player collider, if so, set isGrounded to true
-        grounded = false;
+
+        //Check if any of the overlapping colliders are not player collider, if so, set is_grounded to true
+        _grounded = false;
 
         if (colliders.Length > 0)
         {
             for (int i = 0; i < colliders.Length; i++)
             {
-                if (colliders[i] != _capsuleCollider)
+                if (colliders[i] != _mainCollider)
                 {
-                    grounded = true;
+                    _grounded = true;
                     break;
                 }
             }
         }
-
-        // Apply movement velocity
-        _rb.velocity = new Vector2((moveDirection) * _maxSpeed, _rb.velocity.y);
-
-        // Simple debug
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), grounded ? Color.green : Color.red);
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), grounded ? Color.green : Color.red);
     }
+    #endregion
+
+    #region Movement
+    private void Movement()
+    {
+        CheckGrounded();
+        _moveDirection = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector2(_moveDirection * _maxSpeed, _rb.velocity.y);
+        
+        if(facingRight == false && _moveDirection > 0)
+        {
+            Flip();
+        } 
+        
+        else if(facingRight == true && _moveDirection < 0)
+        {
+            Flip();
+        }
+    }
+    #endregion
+
+    #region Flip
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+    #endregion
+
+    #region Jump
+    private void Jump()
+    {
+        // Jumping
+        if ((Input.GetKeyDown(KeyCode.Space)) && _grounded)
+        {
+            //_rb.velocity = new Vector2(_rb.velocity.x, _jumpHeight);
+            if(_rb.gravityScale > 0)
+            {
+                _rb.velocity = Vector2.up * _jumpHeight;
+            }
+            
+            else if(_rb.gravityScale < 0)
+            {
+                _rb.velocity = Vector2.down * _jumpHeight;
+            }
+        }
+    }
+    #endregion
+
 }
